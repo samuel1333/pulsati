322952db2b054f565b75cd5f3ba07f89cdec366b
samuel1333 <93929797+samuel1333@users.noreply.github.com>
2022-02-23 10:45:37 -0300

    routeando o login

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes as Switch } from 'react-router-dom';
-// import Clients from './pages/Clients';
-// import Blog from './pages/Blog';
-// import Contact from './pages/Contact';
 import Home from './pages/Home';
-// import Partners from './pages/Partners';
-// import Services from './pages/Services';
-// import Who from './pages/Who';
+import Index from './pages/Login/index';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes as Switch } from 'react-router-dom';
+
+// import Who from './pages/Who';
+// import Blog from './pages/Blog';
+// import Clients from './pages/Clients';
+// import Contact from './pages/Contact';
+// import Partners from './pages/Partners';
+// import Services from './pages/Services';
 
 export const Routes = () => {
   return (
@@ -17,6 +19,7 @@ export const Routes = () => {
       <Switch>
         <Route path="/" element={<Home />} exact />
         <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Index />} />
         {/* <Route path="/blog" element={<Blog />} />
           <Route path="/clients" element={<Clients />} />
           <Route path="/contact" element={<Contact />} />
